948d9c2828a86fa15005dc067e0be23fb46a95a9
Fatima zahra majdou <146893864+Fatimazahra-21@users.noreply.github.com>
2023-10-09 01:51:41 +0300

    Create mention.py

---
+++ mention.py
@@ -0,0 +1,15 @@
+N1 = float (input("entrer N1 :"))
+N2 = float (input("entrer N2 :"))
+N3 = float (input("entrer N3 :"))
+moyenne = (N1+N2+N3)/3
+print("la moyenne de étudiant est : ",moyenne)
+if moyenne < 10 :
+print("la mention de étudiant est : inssufisant")
+elif moyenne >= 10 and moyenne < 12 :
+print("la mention de étudiant est : passable")
+elif moyenne >=12 and moyenne < 14 :
+print("la mention de étudiant est : assez bien")
+elif moyenne >=14 and moyenne < 16 :
+print("la mention de étudiant est : bien")
+print("la mention de étudiant est : tres bien")
+
